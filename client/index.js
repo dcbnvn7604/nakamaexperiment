659457bd15b9dd01f5e0a0f81d465bbb46bc7392
Falgor.C <tranhoa.7604@gmail.com>
2022-10-03 14:20:11 +0700

    wip

--- client/index.js
+++ client/index.js
@@ -1,4 +1,9 @@
 import {Client} from "@heroiclabs/nakama-js"
+import {XMLHttpRequest} from 'xmlhttprequest'
+import fetch from 'node-fetch'
+
+global.XMLHttpRequest = XMLHttpRequest
+global.fetch = fetch
 
 var email = 'client1@gmail.com';
 var password = 'abc@1234';
@@ -6,6 +11,8 @@ var password = 'abc@1234';
 async function main() {
 	let client = new Client("defaultkey", "nakama", 7350);
 	let session = await client.authenticateEmail(email, password, true);
+	let account = await client.getAccount(session);
+	console.info(account);
 }
 
 main();
